a5ac2d6170bd4b1dad80f3e608f3d637b39aac5c
袁康 <yuankang@meituan.com>
2017-01-17 10:24:53 +0800

    refactor tpl checked

--- src/checkTpl.js
+++ src/checkTpl.js
@@ -3,7 +3,9 @@
  * 检验模板名是否存在，避免使用不存在的模板名时长时间的下载等待
  */
 
-let request = require('request');
+'use strict';
+
+let axios = require('axios');
 let ora = require('ora');
 let chalk = require('chalk');
 
@@ -12,33 +14,30 @@ let log = require('../src/log');
 module.exports = function (template,officialTemplate,done){
     log.tips();
 
+    template = template.indexOf('/') === -1 ? template : template.split('/')[0];
+
     let spinner = ora({
         text: "checking template...",
         color:"blue"
     }).start();
 
-    request({
+    axios({
         url: 'https://api.github.com/users/waka-templates/repos',
         headers: {
             'User-Agent': 'waka-cli'
         }
-    }, (err, res, body) => {
-        if(err){
-            spinner.text = chalk.red('waka cli:checking template failed.');
-            spinner.fail();
-            process.exit(1);
-        }
-
-        let requestBody = JSON.parse(body);
-        if (Array.isArray(requestBody)) {
+    }).then((res) => {
+        if (Array.isArray(res.data)) {
             let reposName = [];
-            requestBody.forEach(function (repo) {
+
+            res.data.forEach(function (repo) {
                 reposName.push(repo.name);
             });
+
             if(reposName.indexOf(template) > -1){
                 spinner.text = chalk.green('Template checked success.');
                 spinner.succeed();
-                log.tips();
+
                 log.tips();
                 done(officialTemplate);
             } else {
@@ -48,11 +47,19 @@ module.exports = function (template,officialTemplate,done){
                 log.tips(`Please check all available official templates by ${chalk.blue('waka list')} in terminal.`);
             }
         } else {
-            spinner.text = chalk.white('waka cli:checking template failed,error message as follows:');
+            spinner.text = chalk.white('waka cli:checking template failed, error message as follows:');
             spinner.fail();
 
             log.tips();
-            log.error(requestBody.message);
+            log.error(`     ${res.statusText}: ${res.data.message}`);
+        }
+    }).catch((err) => {
+        let res = err.response;
+        if(err){
+            spinner.text = chalk.white('waka cli:checking official template failed, error message as follows:');
+            spinner.fail();
+            log.tips();
+            log.error(`     ${res.statusText}: ${res.data.message}`);
         }
     });
 };
