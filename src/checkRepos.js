541c0931f1a6d72327dbfa9a36945740bc08bd50
袁康 <yuankang@meituan.com>
2017-01-17 10:01:31 +0800

    check repos

---
+++ src/checkRepos.js
@@ -0,0 +1,53 @@
+/**
+ * Created by pomy on 17/01/2017.
+ */
+
+/**
+ * Created by pomy on 16/01/2017.
+ * check the repos is exists on github.com.
+ */
+
+'use strict';
+
+let request = require('request');
+let ora = require('ora');
+let chalk = require('chalk');
+
+let log = require('../src/log');
+
+module.exports = function (repo,done){
+    let spinner = ora({
+        text: 'checking template from github.com...',
+        color:"blue"
+    }).start();
+
+    request({
+        url: `https://github.com/${repo}`,
+        headers: {
+            'User-Agent': 'waka-cli'
+        }
+    }, (err, res) => {
+
+        if(err){
+            spinner.text = chalk.red(`waka cli:checking template ${repo} from github.com failed.`);
+            spinner.fail();
+            process.exit(1);
+        }
+
+        log.tips();
+
+        if(res.statusCode === 200){
+            spinner.text = chalk.green('Template checked success from github.com.');
+            spinner.succeed();
+            log.tips();
+            done(repo);
+        } else {
+            spinner.stop();
+            log.tips();
+            log.tips(chalk.red(`Template checked fail: ${repo} not found on github.com`));
+            log.tips();
+            log.tips(`Please check all available official templates by ${chalk.blue('waka list')} in terminal.`);
+            process.exit(1);
+        }
+    });
+};
