0163e2c5036b110e339f29097ff0aeb41dfa4dbb
袁康 <yuankang@meituan.com>
2017-01-18 14:54:42 +0800

    version checked

---
+++ src/check-version.js
@@ -0,0 +1,67 @@
+/**
+ * Created by pomy on 18/01/2017.
+ * check the cli version
+ */
+
+'use strict';
+
+let semver = require('semver');
+let chalk = require('chalk');
+let axios = require('axios');
+let ora = require('ora');
+
+let pkg = require('../package.json');
+let log = require('./log');
+
+log.tips();
+
+module.exports = function (done) {
+
+    let spinner = ora({
+        text: "checking waka cli version...",
+        color:"blue"
+    }).start();
+
+    if (!semver.satisfies(process.version, pkg.engines.node)) {
+        spinner.text = chalk.white('waka cli:checking waka cli version failed, error message as follows:');
+        spinner.fail();
+
+        log.tips();
+        log.error(`  You must upgrade node to ${pkg.engines.node} to use waka cli`);
+    }
+
+    axios({
+        url: 'http://r.npm.sankuai.com/@mx/waka',
+        method: 'get',
+        timeout: 7000
+    }).then((res) => {
+        if(res.status === 200){
+            spinner.text = chalk.green('Checking waka cli version success.');
+            spinner.succeed();
+
+            let local = pkg.version;
+            let latest = res.data['dist-tags'].latest;
+
+            if (semver.lt(local, latest)) {
+                log.tips();
+                log.tips(chalk.blue('  A newer version of waka cli is available.'));
+                log.tips();
+                log.tips(`  latest:    ${chalk.green(latest)}`);
+                log.tips(`  installed:    ${chalk.red(local)}`)
+                log.tips(`  update waka latest: npm update -g waka`);
+                log.tips();
+            }
+            done();
+        }
+    }).catch((err) => {
+        if(err){
+            let res = err.response;
+
+            spinner.text = chalk.white('waka cli:checking waka cli version failed, error message as follows:');
+            spinner.fail();
+
+            log.tips();
+            log.error(`     ${res.statusText}: ${res.data.message}`);
+        }
+    });
+};
